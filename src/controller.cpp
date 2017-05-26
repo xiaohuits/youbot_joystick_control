23830dcb08ad22097fb4b6bd873239d2996ea32e
HuiXiao <hui.xiao@uconn.edu>
2017-05-26 15:18:23 -0400

    initial connectionestiblished

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,35 +9,50 @@
 
 using namespace std;
 
-geometry_msgs::Twist twistSet(const double& lx, const double& ly, const double& az)
+class SubscriberAndPublish
 {
-	geometry_msgs::Twist msg;
-	msg.linear.x = lx;
-	msg.linear.y = ly;
-	msg.angular.z = az;
-	return msg;
-}
+public:
+	/*void SubscribeAndPublish()
+	{
+		twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
+		sub_to_test = n.subscribe<sensor_msgs::Joy>("joy", 1, &SubscriberAndPublish::movement, this);
+	}
+	*/
+	void movement(const sensor_msgs::Joy::ConstPtr& joy)
+	{
+		geometry_msgs::Twist msg;
+		double lstickh = joy->axes[0];
+		double lstickv = joy->axes[1];
+		double rstickh = joy->axes[3];
+//		ROS_INFO(joy->axes);
+		msg = twistSet(0, lstickv, lstickh);	
+//		arm_position[0] = arm_step_size*joy->axes[4];
+		cout << msg << endl;
+		twist_pub.publish(msg);
+	}
 
-geometry_msgs::Twist movement(const sensor_msgs::Joy::ConstPtr& joy)
-{
-	geometry_msgs::Twist msg;
-	double lstickh = joy->axes[0];
-	double lstickv = joy->axes[1];
-	double rstickh = joy->axes[3];
-//	ROS_INFO(joy->axes);
-	msg = twistSet(0, lstickv, lstickh);	
-//	arm_position[0] = arm_step_size*joy->axes[4];
-	cout << msg << endl;
-//	twist_pub.publish(msg);
-	return msg;
-}
+	geometry_msgs::Twist twistSet(const double& lx, const double& ly, const double& az)
+	{
+		geometry_msgs::Twist msg;
+		msg.linear.x = lx;
+		msg.linear.y = ly;
+		msg.angular.z = az;
+		return msg;
+	}
+private:
+	ros::NodeHandle n;
+	ros::Publisher twist_pub=n.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	ros::Subscriber sub_to_test=n.subscribe<sensor_msgs::Joy>("joy", 1, &SubscriberAndPublish::movement, this);
+
+};
+
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "controller");
-	ros::NodeHandle n;
-	ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
-	ros::Subscriber sub_to_test = n.subscribe<sensor_msgs::Joy>("joy", 1, movement);
+	
+	SubscriberAndPublish SAPObject;
+
 	ros::spin();
 	return 0;
 }
